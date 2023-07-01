6a20550f0e57a9f09d6398f199fb2c697e21ffc5
Gary Hester <ghester@codefool.org>
2023-07-01 13:47:15 -0500

    Added extern reference for build_info

--- include/buildinfo.h
+++ include/buildinfo.h
@@ -6,6 +6,8 @@ struct build_info {
     std::string app_name = "libcf - library of codefool objects";
     int ver_major = 0;
     int ver_minor = 0;
-    int ver_build = 1;
+    int ver_build = 2;
     friend std::ostream& operator<< (std::ostream& os, const build_info& bi);
 };
+
+extern const build_info bi;
